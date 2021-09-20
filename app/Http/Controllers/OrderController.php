d7f29c6d74ddfc06bd2bdebb5075793beb8fd27c
akasz56 <indo14nurfath@apps.ipb.ac.id>
2021-09-21 06:37:49 +0700

    ordercontroller tests

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FlowerSize;
+use App\Models\FlowerType;
+use App\Models\FlowerWrap;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function test(Request $request)
     {
-        dd($request);
+        $request->validate([
+            'ID' => 'required',
+            'type' => 'required',
+            'wrap' => 'required',
+            'size' => 'required',
+        ]);
+        
+        $product = Product::where('publicID',$request->ID)->first();
+        $type = FlowerType::where('productID',$request->ID)->where('name', $request->type)->first();
+        $wrap = FlowerWrap::where('productID',$request->ID)->where('name', $request->wrap)->first();
+        $size = FlowerSize::where('productID',$request->ID)->where('name', $request->size)->first();
+
+        dump($type);
+        dump($wrap);
+        dump($size);
+        dd($product);
     }
 }
